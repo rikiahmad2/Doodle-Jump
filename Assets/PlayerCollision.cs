ebefb9c12f9a9e2df0653943c8c1d0dac27628bd
Asep Darmawan <asep.darmawan@agate.id>
2020-02-24 19:27:24 +0700

    first commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Player movement;
+    //public Text text;
+    private Transform posisi;
+    private BoxCollider2D myColid;
+    private bool isDead = false;
+    private Rigidbody2D rb;
+    //private Color test;
+
+    void Start()
+    {
+        myColid = GetComponent<BoxCollider2D>();
+        posisi = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Rintangan")
+        {
+            movement.enabled = false;
+            myColid.enabled = false;
+            isDead = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (isDead)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            posisi.Rotate(Vector3.forward*Time.deltaTime*500f);
+        }  
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class movement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float movementS = 10f;
+    float gerak= 0f;
+    float jump = 5f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gerak = Input.GetAxis("Horizontal") * movementS;
+	}
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = gerak;
+        rb.velocity = velocity;
+        if (Input.GetButtonDown("W"))
+        {
+            velocity = rb.velocity;
+            velocity.y = jump;
+            rb.velocity = velocity;
+        }
+    }
+}
